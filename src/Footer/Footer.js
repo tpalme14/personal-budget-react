11c2d0ccc670a43da7053087776d38f563e58d8c
Taylor Palmer <tpalme14@uncc.edu>
2024-09-27 13:19:49 -0400

    Brought Personal Budget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+    <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+    </footer>
+    );
+  }
+  
+  export default Footer;
+  
---
+++ src/Menu/Menu.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Menu() {
+    return (
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="about">About</Link></li>
+            <li><Link to="login">Login</Link></li>
+        </ul>
+    </nav>
+    );
+  }
+  
+  export default Menu;
+  
